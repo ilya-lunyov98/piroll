af603204736d9ba602b62cc43266ba6ef500268c
ilya-lunyov98 <ilya.lunyov.98@gmail.com>
2020-04-16 21:47:51 +0300

    added form

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,19 @@
-function ibg(){
+$(document).ready(function(){
+
+    function ibg(){
 
     $.each($('.ibg'), function(index, val) {
     if($(this).find('img').length>0){
     $(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
     }
     });
-    
-}
+    }
+
+    $('.slider__container').slick({
+      arrows: false,
+      dots: true,
+    //   autoplay: true,
+      autoplaySpeed: 2000
+    });
+
+  });
